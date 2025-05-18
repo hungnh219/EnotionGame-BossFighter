dce817f7be450702bb1e78e224830a1064c6238c
HoangHung <hungnh219@gmail.com>
2025-05-19 00:17:42 +0700

    [Task3]: https://github.com/hungnh219/EnotionGame-BossFighter/issues/3 [Done]: - Chỉnh sửa hình ảnh hiển thị theo kích thước - Di chuyển nhân vật bằng phím (không dùng hàm update)

--- BossFighter/assets/scripts/Game.js
+++ BossFighter/assets/scripts/Game.js
@@ -13,14 +13,23 @@ cc.Class({
 
         bossAttackAnimation: cc.Animation,
         mapLayout: cc.Layout,
+
+        playerPrefab: cc.Prefab,
+        playerSpawn: cc.Node,
+
+        enemyPrefab: cc.Prefab,
+        enemySpawn: cc.Node,
     },
 
     // LIFE-CYCLE CALLBACKS:
 
     onLoad () {
-        // GameController.getInstance().testPrintScene("hihi hehehe");
-        // GameController.getInstance().printEditBoxValue();
+        // variables
         this.gridMap = [];
+        this.isMoving = false;
+        this.pressedKeys = new Set();
+
+
 
         for (let j = 0; j < this.mapHeight; j++) {
             let row = [];
@@ -35,6 +44,9 @@ cc.Class({
 
             this.gridMap.push(row);
         }
+
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_UP, this.onKeyUp, this);
     },
 
     start () {
@@ -43,27 +55,80 @@ cc.Class({
     },
 
     // update (dt) {},
+    onDestroy() {
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_DOWN, this.keyboardInput, this);
+        cc.systemEvent.off(cc.SystemEvent.EventType.KEY_UP, this.keyboardInput, this);
+    },
+
+    onKeyDown(event) {
+        this.pressedKeys.add(event.keyCode);
+
+        // delay to wait set of keys down
+        this.scheduleOnce(() => {
+            this.checkMove();
+        }, 0.1);
+    },
+    onKeyUp(event) {
+        this.pressedKeys.delete(event.keyCode);
+    },
+    checkMove() {
+        if (this.isMoving) return;
+
+        let dx = 0;
+        let dy = 0;
+
+        if (this.pressedKeys.has(cc.macro.KEY.w) || this.pressedKeys.has(cc.macro.KEY.up)) {
+            dy += 1;
+        }
+        if (this.pressedKeys.has(cc.macro.KEY.s) || this.pressedKeys.has(cc.macro.KEY.down)) {
+            dy -= 1;
+        }
+        if (this.pressedKeys.has(cc.macro.KEY.a) || this.pressedKeys.has(cc.macro.KEY.left)) {
+            dx -= 1;
+        }
+        if (this.pressedKeys.has(cc.macro.KEY.d) || this.pressedKeys.has(cc.macro.KEY.right)) {
+            dx += 1;
+        }
+
+        if (dx !== 0 || dy !== 0) {
+            this.moveCharacter(dx, dy);
+        }
+    },
+    moveCharacter (dx, dy) {
+        const newX = this.bossAttackAnimation.node.x + dx * this.mapTileWidth;
+        const newY = this.bossAttackAnimation.node.y + dy * this.mapTileHeight;
+
+        // check if the new position is walkable
+        // if (newX < 0 || newX >= this.mapWidth * this.mapTileWidth) return;
+
+        this.isMoving = true;
+        const moveAction = cc.moveTo(0.5, newX, newY);
+        // const moveAction = cc.moveTo(0.5, newX, newY).easing(cc.easeCubicActionOut());
+        const finishCallback = cc.callFunc(() => {
+            this.isMoving = false;
+            this.checkMove();
+        });
+        const sequence = cc.sequence(moveAction, finishCallback);
+        this.bossAttackAnimation.node.runAction(sequence);
+
+    },
     testChangeScene () {
         console.log("testChangeScene");
-        // cc.director.loadScene("BossFighter");
-
         this.initMapView();
+
+        
     },
 
     initMapView() {
         this.mapLayout.node.removeAllChildren();
 
-        // center the map
+        // create grid map
+            // center the map
         this.mapLayout.node.x = -this.mapWidth * this.mapTileWidth / 2;
         this.mapLayout.node.y = -this.mapHeight * this.mapTileHeight / 2;
 
-        console.log("initMapView", this.mapHeight, this.mapWidth);
         this.mapLayout.node.width = this.mapWidth * this.mapTileWidth;
         this.mapLayout.node.height = this.mapHeight * this.mapTileHeight;
-
-
-        console.log("this.mapLayout.node.width", this.mapLayout.node.width);
-        console.log("this.mapLayout.node.height", this.mapLayout.node.height);
 
         for (let j = 0; j < this.mapHeight; j++) {
             for (let i = 0; i < this.mapWidth; i++) {
@@ -83,10 +148,10 @@ cc.Class({
             }
         }
 
+            // create boss attack animation (test)
         const size = 2;
-
-        const posX = 2;
-        const posY = 7;
+        const posX = 1;
+        const posY = 5;
 
         this.addObjectIntoMap(posX, posY, size, this.bossAttackAnimation);
         if (this.gridMap[posX][posY].object !== null) {
@@ -106,13 +171,28 @@ cc.Class({
         let objectSprite = objectNode.getComponent(cc.Sprite);
         objectSprite.sizeMode = cc.Sprite.SizeMode.CUSTOM;
 
+
+        // resize the object
+        if (objectNode.width > objectNode.height) {
+            console.log("objectSprite", "width > height", objectNode.width, objectNode.height);
+            let ratio = this.mapTileHeight / objectNode.height;
+
+            objectNode.height = this.mapTileHeight * size;
+
+            objectNode.width *= ratio * size;
+            console.log("after resize", objectNode.width, objectNode.height);
+        } else {
+            console.log("objectSprite", "width < height", objectNode.width, objectNode.height);
+            objectNode.width = this.mapTileWidth * size;
+
+            objectNode.height = this.mapTileHeight * size * (objectNode.width / this.mapTileWidth);
+            console.log("after resize", objectNode.width, objectNode.height);
+        }
+
+        // set the position of the object
         const mapPos = this.mapLayout.node.getPosition();
-        objectNode.x = mapPos.x + gridX * this.mapTileWidth;
-        objectNode.y = mapPos.y + gridY * this.mapTileHeight;
-
-
-        // calculate the size of the object (calculate based on raw size and scale it)
-
+        objectNode.x = mapPos.x + gridX * this.mapTileWidth + (this.mapTileWidth * size)/ 2;
+        objectNode.y = mapPos.y + gridY * this.mapTileHeight + (this.mapTileHeight * size)/ 2;
 
         if (this.gridMap[gridX][gridY].object === null) {
             this.gridMap[gridX][gridY].object = object;
