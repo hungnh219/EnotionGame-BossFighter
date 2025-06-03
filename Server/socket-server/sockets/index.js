21ef9c2927cec2b2f22a5fb60b9e544c16674591
Fresher-Cocos-Chiet-08 <trietnguyenhoangminh@gmail.com>
2025-06-03 16:55:46 +0700

    add from server

--- Server/socket-server/sockets/index.js
+++ Server/socket-server/sockets/index.js
@@ -1,8 +1,10 @@
 // import mapEvents from "./map";
 const mapEvents = require('./map');
+const roomEvents = require('./room')
 
 function registerSocketEvents(io, socket) {
     mapEvents(io, socket);
+    roomEvents(io, socket);
 }
 
 module.exports = registerSocketEvents;
